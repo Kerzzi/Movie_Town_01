8cd4637cfec63e85e93ce263cc00e8157aaf8fe5
Kerzzi <kerzzi@outlook.com>
2017-03-29 19:54:52 +0800

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  module ApplicationHelper
+
+    # 根据所在的页面返回完整的标题
+    def full_title(page_title = '')
+      base_title = "Movie Town"
+      if page_title.empty?
+        base_title
+      else
+        page_title + " | " + base_title
+      end
+    end
+  end
 end
