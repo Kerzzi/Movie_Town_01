c2b56ef65ccc23aabe1aa8385191455b0c132d83
Kerzzi <kerzzi@outlook.com>
2017-03-29 19:44:25 +0800

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
-
   get 'static_pages/help'
+  get  'static_pages/about'
 
-  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
