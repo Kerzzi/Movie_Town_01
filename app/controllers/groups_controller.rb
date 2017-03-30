a74486617e3aba515872d7e3b669996e1b92704a
Kerzzi <kerzzi@outlook.com>
2017-03-30 19:22:10 +0800

    show group's creator info

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create]
+  before_action :logged_in_user , only: [:new, :create]
 
   def index
     @groups = Group.all
