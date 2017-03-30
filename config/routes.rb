66248b57e82155989a3a380fbfe0f8906798d54f
Kerzzi <kerzzi@outlook.com>
2017-03-30 19:40:14 +0800

    add posts nested route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :groups
+  resources :groups do
+    resources :posts
+  end
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
